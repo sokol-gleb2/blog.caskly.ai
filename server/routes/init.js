bb3929b977b5dc06d5c697576104c2769532daa4
Gleb Sokolovski <h.sokolovskyi@sms.ed.ac.uk>
2026-02-05 22:49:32 +0000

    init

---
+++ server/routes/init.js
@@ -0,0 +1,10 @@
+import 'dotenv';
+import { express } from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
